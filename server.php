32a9f0569e176408ea8a4782c28aa69bd5d686c0
The Touch <01ttouc@gmail.com>
2011-10-11 22:22:33 +0300

    changed username var from $usr[$i] to $usr[$i]["name"] to resolve prompt conflict

--- server.php
+++ server.php
@@ -14,7 +14,8 @@ $conf=array("addr"=>"127.0.0.1",
 	"port"=>4444, // Port to listen to
 	"max"=>20, // Maximum number of 
 	"guest"=>true, // Allow guests
-	"active"=>2 // 0=no registration, 1=activation by admin, 2=activation by e-mail, 3=no activation
+	"active"=>2, // 0=no registration, 1=activation by admin, 2=activation by e-mail, 3=no activation
+	"version"=>"1.02b"
 );
 // Command list
 $cm=array('h'=>"help", // User command list
@@ -62,6 +63,7 @@ $help.="\n -a|--address [addr]	Define the address to listen";
 $help.="\n -g|--guest		Enable or disable guest login";
 $help.="\n -e|--enc		Encrypted connections*";
 $help.="\n -d|--dev		Debugging mode**";
+$help.="\n -s|--setup		Setup the database, will create tables needed by server (it won't delete anything)";
 $help.="\n -h|--help		Display this screen";
 $help.="\n\n*: By encrypting the connection, the text sent is encrypted, NOT SSL connection...";
 $help.="\n**: In this mode, if you connect to socket 0, you will be logged in, as admin\n\twithout ANY authentication";
@@ -132,19 +134,16 @@ if($conf['dev']==true) outp("Server in devlopment mode (socket ".$conf["devs"]."
 while(true){
 	socket_set_block($sock);
 	$read[0]=$sock; // Setup clients listen socket for reading
-	for($i=0;$i<$conf['max'];$i++){
-		if($client[$i]['sock']!=null)
-			$read[$i+1]=$client[$i]['sock'];
-	}
+	for($i=0;$i<$conf['max'];$i++) if($client[$i]['sock']!=null) $read[$i+1]=$client[$i]['sock'];
 	$ready=socket_select($read,$write=NULL,$except=NULL,$tv_sec=NULL);
 
 	// If a new connection is being made add it to the clients array
 	if(in_array($sock,$read)){
 		for($i=0;$i<$conf['max'];$i++){
 			if($client[$i]['sock']==null){
-				if(!$client[$i]['sock']=socket_accept($sock)){
+				if(!$client[$i]['sock']=socket_accept($sock))
 					outp("socket_accept() failed: ".socket_strerror($client[$i]['sock']),'x');
-				}elseif($i==0 && $conf['dev']==true) outp("Client connected at socket ".$i." (admin)",'!');
+				elseif($i==0 && $conf['dev']==true) outp("Client connected at socket ".$i." (admin)",'!');
 				else outp("Client connected at socket ".$i,'!');
 				break;
 			}elseif($i==$conf['max']-1) outp("Too many clients",'x');
@@ -154,55 +153,54 @@ while(true){
 
 	for($i=0;$i<$conf['max'];$i++){
 		if(in_array($client[$i]['sock'],$read)){
-			unset($usr[$i]["prom"]);
-			$usr[$i]["prom"]=$usr[$i].$conf["prom"];
 			if($conf['dev']==true) $usr[$conf["devs"]]="admin";
-			if(isset($usr[$i])){
+			if(isset($usr[$i]["name"])){
 				$input=socket_cread($client[$i]['sock']);
 				$n=trim($input);
 				$com=split(" ",$n);
-				if($input==NULL) unset($client[$i]);
 				$nopt=str_getopts($n,1);
 				$admopt=str_getopts($n,3);
 			}
 
-			if(!isset($usr[$i]) || $usr[$i]=="ERR" || $n==$conf['comm'].$cm['l'] && !isset($login[$i])){
+			if(!isset($usr[$i]["name"]) || $usr[$i]["name"]=="ERR" || $n==$conf['comm'].$cm['l'] && !isset($login[$i])){
 				if($conf["guest"]==false || $n==$conf['comm'].$cm['l']){
 					socket_cwrite($client[$i]['sock'],"Login: ");
 					$login[$i]=socket_cread($client[$i]['sock']);
 					socket_cwrite($client[$i]['sock'],"Pass: ");
 					$pass[$i]=socket_cread($client[$i]['sock']);
 
-					if(check($login[$i],$pass[$i],$client[$i]['sock'])!="ERR")
-						$usr[$i]=trim(check($login[$i],$pass[$i],$client[$i]['sock']));
+					if(check($login[$i],$pass[$i],$client[$i]['sock'])!="ERR") {
+						$usr[$i]["name"]=trim(check($login[$i],$pass[$i],$client[$i]['sock']));
 						if(doubleu($i)=="ERR") {
 							socket_close($client[$i]['sock']);
 							unset($client[$i]['sock']);
 							outp("Double login! Socket: ".$i.", User: ".strip($login[$i]),'x');
 						}else{
-							socket_cwrite($client[$i]['sock'],"\nWelcome ".$usr[$i]."!\n");
+							socket_cwrite($client[$i]['sock'],"\nWelcome ".$usr[$i]["name"]."!\n");
 							socket_cwrite($client[$i]['sock'],"For help, type \$help\n");
-							$usr[$i]["prom"]=$usr[$i].$conf["prom"];
-							socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
-							for($x=0;$x<$conf['max'];$x++){
+							$usr[$i]["prom"]=$usr[$i]["name"].$conf["prom"];
+							for($x=0;$x<count($client);$x++){
 								if($client[$x]['sock']!=$client[$i]['sock']){
-					socket_cwrite($client[$x]['sock'],"\n\tServer: User ".$usr[$i]." connected!\n");
+					socket_cwrite($client[$x]['sock'],"\n\tServer: User ".$usr[$i]["name"]." connected!\n");
 					socket_cwrite($client[$x]['sock'],$usr[$x]["prom"]);
 								}
 							}
+						}
+					}
 				}else{
-					$usr[$i]="guest".$i;
-					socket_cwrite($client[$i]['sock'],"\nWelcome ".$usr[$i]."!\n");
+					$usr[$i]["name"]="guest".$i;
+					$usr[$i]["prom"]=$usr[$i]["name"].$conf["prom"];
+					socket_cwrite($client[$i]['sock'],"\nWelcome ".$usr[$i]["name"]."!\n");
 					socket_cwrite($client[$i]['sock'],"For help, type \$help\n");
 				}
-			}elseif($n==null && isset($usr[$i]))
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
+			}elseif($n==null && isset($usr[$i]["name"]))
+				NULL;
 			elseif($n==$conf['comm'].$cm['o']){
 				unset($login[$i]);
-				outp("User ".strip($usr[$i])." logged out",'i');
-				$usr[$i]="guest".$i;
-				$usr[$i]["prom"]=$usr[$i].$conf["prom"];
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
+				outp("User ".strip($usr[$i]["name"])." logged out",'i');
+				$usr[$i]["name"]="guest".$i;
+				$usr[$i]["prom"]=$usr[$i]["name"].$conf["prom"];
+
 			}elseif($n==$conf['comm'].$cm['r']){
 				socket_cwrite($client[$i]['sock'],"\nUsername: ");
 				$user=socket_cread($client[$i]['sock']);
@@ -227,16 +225,14 @@ while(true){
 	socket_cwrite($client[$i]['sock'],"\n|  ".$conf['comm'].$cm['o']."    | Logout from the server\t\t\t|");
 	socket_cwrite($client[$i]['sock'],"\n|  ".$conf['comm'].$cm['q']."    | Close the connection\t\t\t|");
 	socket_cwrite($client[$i]['sock'],"\n+-=-=-=-=-=-+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 			}elseif($n==$conf['comm'].$cm['i']){
 				// Info requested
-				socket_cwrite($client[$i]['sock'],"BrainF chat server v1.00b by ttouch\n");
-				socket_cwrite($client[$i]['sock'],"0nLine now:\n");
-				for($x=0;$x<$conf['max'];$x++){
-					if(isset($usr[$x])) socket_cwrite($client[$i]['sock'],"\t".$usr[$x]."\n");
+				socket_cwrite($client[$i]['sock'],"BrainF chat server v.".$conf['version']." by ttouch\n");
+				socket_cwrite($client[$i]['sock'],"Online now:\n");
+				for($x=0;$x<count($client);$x++){
+					if(isset($usr[$x]["name"])) socket_cwrite($client[$i]['sock'],"\t".$usr[$x]["name"]."\n");
 				}
 				socket_cwrite($client[$i]['sock'],"For help type ".$conf['comm']."help\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 			}elseif(strstr($nopt[0],"@")){
 				if(in_array(str_replace("@","",$nopt[0]),$usr)){
 					// Private message
@@ -244,7 +240,6 @@ while(true){
 					$pm=PM($output,$i);
 					if($pm=="ERR") socket_cwrite($client[$i]['sock'],"\tMessage could not be sent\n");
 				}else socket_cwrite($client[$i]['sock'],"\t".$nopt[0]." is not a command  or username\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 			}elseif($n==$conf['comm'].$cm['u']){
 				// Username change requested
 				socket_cwrite($client[$i]['sock'],"Password: ");
@@ -255,19 +250,18 @@ while(true){
 				$newu2=socket_cread($client[$i]['sock']);
 
 				if($newu==$newu2) {
-					$ch=newdt($usr[$i],$oldp,$newu);
+					$ch=newdt($usr[$i]["name"],$oldp,$newu);
 					if($ch=="OK"){
 						socket_cwrite($client[$i]['sock'],"Username changed!\n");
-						$usr[$i]=$newu;
+						$usr[$i]["name"]=$newu;
 					}elseif ($ch=="ERR1") socket_cwrite($client[$i]['sock'],"Empty username or password given!\n");
 					else socket_cwrite($client[$i]['sock'],"Error on username change!\n");
 				}else socket_cwrite($client[$i]['sock'],"Usernames do not match!\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 			}elseif($n==$conf['comm'].$cm['p']){
 				// Password change requested
 				socket_cwrite($client[$i]['sock'],"Old password: ");
 				$oldp=socket_cread($client[$i]['sock']);
-				if(check($usr[$i],$oldp,$client[$i]['sock'],"pass")=="OK"){
+				if(check($usr[$i]["name"],$oldp,$client[$i]['sock'],"pass")=="OK"){
 					socket_cwrite($client[$i]['sock'],"New password: ");
 					$newp=socket_cread($client[$i]['sock']);
 					socket_cwrite($client[$i]['sock'],"Retype new password: ");
@@ -277,13 +271,12 @@ while(true){
 					break;
 				}
 				if($newp==$newp2 && $err!=1) {
-					$ch=newdt($usr[$i],$oldp,NULL,$newp);
+					$ch=newdt($usr[$i]["name"],$oldp,NULL,$newp);
 					if($ch=="OK"){
 						socket_cwrite($client[$i]['sock'],"Password changed!\n");
-						$usr[$i]=NULL;
+						$usr[$i]["name"]=NULL;
 					}else socket_cwrite($client[$i]['sock'],"Error on password change!\n");
 				}else socket_cwrite($client[$i]['sock'],"Passwords do not match!\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 			}elseif($n==$conf['comm'].$cm['q']){
 				if($client[$i]['sock']!=null){
 					// Disconnect requested
@@ -291,24 +284,24 @@ while(true){
 					unset($client[$i]['sock']);
 					outp("Client disconnected at socket ".$i,'x');
 					for($x=0;$x<count($client);$x++){
-						socket_cwrite($client[$x]['sock'],"\n\t".$usr[$i]." disconnected!\n");
+						socket_cwrite($client[$x]['sock'],"\n\t".$usr[$i]["name"]." disconnected!\n");
 						socket_cwrite($client[$x]['sock'],$usr[$i]["prom"]);
 					}
-					$usr[$i]=NULL;
+					$usr[$i]["name"]=NULL;
 					if($i==$adm) $adm=-1;
 				}
 			}elseif(strstr($nopt[0],$conf['comm']) && !in_array(str_replace($conf['comm'],"",$n),$cm)){
 				socket_cwrite($client[$i]['sock'],"\tServer: ".$nopt[0]." is not a command  or username\n");
 			//	Begin Admin Section	//
-			}elseif($admopt[0]==$conf['acomm'].$cm['a']['k'] && in_array($usr[$i],$admins)){
+			}elseif($admopt[0]==$conf['acomm'].$cm['a']['k'] && in_array($usr[$i]["name"],$admins)){
 				// Terminate server
-				for($x=0;$x<$conf['max'];$x++){
+				for($x=0;$x<count($client);$x++){
 					socket_cwrite($client[$x]['sock'],"\nAdmin terminated the server...\n");
 				}
 				socket_close($sock);
 				unset($sock);
 				outp("Server terminated by admin",'e');
-			}elseif($admopt[0]==$conf['acomm'].$cm['a']['h'] && in_array($usr[$i],$admins)){
+			}elseif($admopt[0]==$conf['acomm'].$cm['a']['h'] && in_array($usr[$i]["name"],$admins)){
 				socket_cwrite($client[$i]['sock'],"\n\t\t+-=-=-=-=-+");
 				socket_cwrite($client[$i]['sock'],"\n\t\t| Select  |");
 				socket_cwrite($client[$i]['sock'],"\n\t\t+-=-=-=-=-+");
@@ -318,43 +311,39 @@ while(true){
 				socket_cwrite($client[$i]['sock'],"\n\t\t| ".$cm['a']['p']." <user> <pass>|");
 				socket_cwrite($client[$i]['sock'],"\n\t\t| ".$cm['a']['u']." <user> <newuser>|");
 				socket_cwrite($client[$i]['sock'],"\n\t\t+-=-=-=-=-+\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
-			}elseif($admopt[0]==$conf['acomm'].$cm['a']['m'] && in_array($usr[$i],$admins)){
+			}elseif($admopt[0]==$conf['acomm'].$cm['a']['m'] && in_array($usr[$i]["name"],$admins)){
 				$add=newdt(NULL,NULL,$admopt[1],$admopt[2],"amake");
 				if($add=="OK") socket_cwrite($client[$i]['sock'],"User ".trim($admopt[1])." created\n");
 				else socket_cwrite($client[$i]['sock'],"Error on user creation!");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 
-			}elseif($admopt[0]==$conf['acomm'].$cm['a']['r'] && in_array($usr[$i],$admins)){
+			}elseif($admopt[0]==$conf['acomm'].$cm['a']['r'] && in_array($usr[$i]["name"],$admins)){
 				$ch=newdt($user,"i'm",$admopt[1],"delete");
 				if($ch=="OK") socket_cwrite($client[$i]['sock'],"User removed!\n");
 				else socket_cwrite($client[$i]['sock'],"Error!\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 
-			}elseif($admopt[0]==$conf['acomm'].$cm['a']['p'] && in_array($usr[$i],$admins)){
+			}elseif($admopt[0]==$conf['acomm'].$cm['a']['p'] && in_array($usr[$i]["name"],$admins)){
 				$ch=newdt($admopt[1],"i'm","admin",$admopt[2]);
 				if($ch=="OK") socket_cwrite($client[$i]['sock'],"Password changed!\n");
 				else socket_cwrite($client[$i]['sock'],"Error!\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 
-			}elseif($admopt[0]==$conf['acomm'].$cm['a']['u'] && in_array($usr[$i],$admins)){
+			}elseif($admopt[0]==$conf['acomm'].$cm['a']['u'] && in_array($usr[$i]["name"],$admins)){
 				$ch=newdt($admopt[1],"i'm",$admopt[2],"admin");
 				if($ch=="OK") socket_cwrite($client[$i]['sock'],"Username changed!\n");
 				else socket_cwrite($client[$i]['sock'],"Error!\n");
-				socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 			//	End Admin Section	//
 
 			}elseif($input){
 				$output=ereg_replace("[\t\n\r]","",$input).chr(0);
 				if($input!=NULL){
-					for($x=0;$x<$conf['max'];$x++){
-						if($client[$x]['sock']!=$client[$i]['sock'])
-						socket_cwrite($client[$x]['sock'],"\n\t".$usr[$i].": ".$output."\n");
-						else socket_cwrite($client[$x]['sock'],"\tYou: ".$output."\n");
-						socket_cwrite($client[$x]['sock'],$usr[$i]["prom"]);
+					for($x=0;$x<count($client);$x++){
+						if($client[$x]['sock']!=$client[$i]['sock']){
+						socket_cwrite($client[$x]['sock'],"\n\t".$usr[$i]["name"].": ".$output."\n");
+						socket_cwrite($client[$x]['sock'],$usr[$x]["prom"]);
+						}else socket_cwrite($client[$i]['sock'],"\n\tYou: ".$output."\n");
 					}
 				}
 			}
+			socket_cwrite($client[$i]['sock'],$usr[$i]["prom"]);
 		}
 	}
 }
